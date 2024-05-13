fefc6ffb8c913e8429493c28dd53afd42acd2106
mnordest <matthiasnordest@gmail.com>
2024-05-13 14:16:25 +0200

    minor fixes

--- Controller/MenuController.php
+++ Controller/MenuController.php
@@ -16,7 +16,7 @@ class MenuController extends BaseAdminController
     public function loadMenuItems() : array
     {
         $stub = [
-            ['title' => 'Parent 1', 'children' => [['title' => 'Child 1.1', 'depth' => 1], ['title' => 'Child 1.2', 'depth' => 1, 'children' => [['title' => 'Child 1.2.1', 'depth' => 2], ['title' => 'Child 1.2.2', 'depth' => 2]]]], 'depth' => 0],
+            ['title' => 'Parent 1', 'children' => [['title' => 'Child 1.1', 'children' => [['title' => 'Child 1.1.1', 'depth' => 2]], 'depth' => 1], ['title' => 'Child 1.2', 'depth' => 1, 'children' => [['title' => 'Child 1.2.1', 'depth' => 2], ['title' => 'Child 1.2.2', 'depth' => 2]]]], 'depth' => 0],
             ['title' => 'Parent 2', 'children' => [['title' => 'Child 2.1', 'depth' => 1], ['title' => 'Child 2.2', 'depth' => 1]], 'depth' => 0],
             ['title' => 'Parent 3', 'depth' => 0]
         ];
