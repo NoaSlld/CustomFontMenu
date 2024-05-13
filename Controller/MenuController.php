7647e54e0899bd250477124394a411ebd902cba5
mnordest <matthiasnordest@gmail.com>
2024-05-13 13:49:59 +0200

    begin of data initialization with the controller

--- Controller/MenuController.php
+++ Controller/MenuController.php
@@ -13,8 +13,16 @@ class MenuController extends BaseAdminController
     }
 
     // Load the menu items
-    public function loadMenuItems() : void
+    public function loadMenuItems() : array
     {
+        $stub = [
+            ['title' => 'Parent 1', 'children' => [['title' => 'Child 1.1', 'depth' => 1], ['title' => 'Child 1.2', 'depth' => 1, 'children' => [['title' => 'Child 1.2.1', 'depth' => 2], ['title' => 'Child 1.2.2', 'depth' => 2]]]], 'depth' => 0],
+            ['title' => 'Parent 2', 'children' => [['title' => 'Child 2.1', 'depth' => 1], ['title' => 'Child 2.2', 'depth' => 1]], 'depth' => 0],
+            ['title' => 'Parent 3', 'depth' => 0]
+        ];
 
+        $dataToLoad = $stub;
+
+        return $dataToLoad;
     }
 }
--- Hook/ConfigHook.php
+++ Hook/ConfigHook.php
@@ -4,10 +4,16 @@ namespace CustomFrontMenu\Hook;
 
 use Thelia\Core\Event\Hook\HookRenderEvent;
 use Thelia\Core\Hook\BaseHook;
+use CustomFrontMenu\Controller\MenuController;
 
 class ConfigHook extends BaseHook
 {
     public function onModuleConfiguration(HookRenderEvent $event){
-        $event->add($this->render("config/module-config.html"));
+
+        $controller = new MenuController;
+
+        $dataToLoad = $controller->loadMenuItems();
+
+        $event->add($this->render("config/module-config.html", ['items' => $dataToLoad]));
     }
 }
