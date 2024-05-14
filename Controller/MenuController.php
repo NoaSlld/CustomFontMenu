57a0fb846f1e512a1d8f19d1371fa92336c517db
mathisdlg <delage.mathis.1@gmail.com>
2024-05-14 12:56:48 +0200

    Change children to childrens

--- Controller/MenuController.php
+++ Controller/MenuController.php
@@ -16,8 +16,8 @@ class MenuController extends BaseAdminController
     public function loadMenuItems() : void
     {
         $stub = [
-            ['id' => '1', 'title' => 'Parent 1', 'children' => [['id' => '2', 'title' => 'Child 1.1', 'children' => [['id' => '3', 'title' => 'Child 1.1.1', 'depth' => 2]], 'depth' => 1], ['id' => '4', 'title' => 'Child 1.2', 'depth' => 1, 'children' => [['id' => '5', 'title' => 'Child 1.2.1', 'depth' => 2], ['id' => '6', 'title' => 'Child 1.2.2', 'depth' => 2]]]], 'depth' => 0],
-            ['id' => '7', 'title' => 'Parent 2', 'children' => [['id' => '8', 'title' => 'Child 2.1', 'depth' => 1], ['id' => '9', 'title' => 'Child 2.2', 'depth' => 1]], 'depth' => 0],
+            ['id' => '1', 'title' => 'Parent 1', 'childrens' => [['id' => '2', 'title' => 'Child 1.1', 'childrens' => [['id' => '3', 'title' => 'Child 1.1.1', 'depth' => 2]], 'depth' => 1], ['id' => '4', 'title' => 'Child 1.2', 'depth' => 1, 'childrens' => [['id' => '5', 'title' => 'Child 1.2.1', 'depth' => 2], ['id' => '6', 'title' => 'Child 1.2.2', 'depth' => 2]]]], 'depth' => 0],
+            ['id' => '7', 'title' => 'Parent 2', 'childrens' => [['id' => '8', 'title' => 'Child 2.1', 'depth' => 1], ['id' => '9', 'title' => 'Child 2.2', 'depth' => 1]], 'depth' => 0],
             ['id' => '10', 'title' => 'Parent 3', 'depth' => 0]
         ];
 
