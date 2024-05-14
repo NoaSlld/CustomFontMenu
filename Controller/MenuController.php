e4d32ce6cb7f23d929c36ad70d87bc12fe4602fb
mathisdlg <delage.mathis.1@gmail.com>
2024-05-14 15:07:39 +0200

    Change the test menu and make it cleaner

--- Controller/MenuController.php
+++ Controller/MenuController.php
@@ -15,9 +15,41 @@ class MenuController extends BaseAdminController
     // Load the menu items
     public function loadMenuItems() : void
     {
-        $stub = [
-            ['id' => '1', 'title' => 'Parent 1', 'childrens' => [['id' => '2', 'title' => 'Child 1.1', 'childrens' => [['id' => '3', 'title' => 'Child 1.1.1', 'depth' => 2]], 'depth' => 1], ['id' => '4', 'title' => 'Child 1.2', 'depth' => 1, 'childrens' => [['id' => '5', 'title' => 'Child 1.2.1', 'depth' => 2], ['id' => '6', 'title' => 'Child 1.2.2', 'depth' => 2]]]], 'depth' => 0],
-            ['id' => '7', 'title' => 'Parent 2', 'childrens' => [['id' => '8', 'title' => 'Child 2.1', 'depth' => 1], ['id' => '9', 'title' => 'Child 2.2', 'depth' => 1]], 'depth' => 0],
+        $stub = 
+        [
+            [
+                'id' => '1', 'title' => 'Parent 1', 'depth' => 0, 'childrens' => 
+                [
+                    [
+                        'id' => '2', 'title' => 'Child 1.1', 'depth' => 1, 'childrens' => 
+                        [
+                            [
+                                'id' => '3', 'title' => 'Child 1.1.1', 'depth' => 2
+                            ]
+                        ]
+                    ],
+                    [
+                        'id' => '4', 'title' => 'Child 1.2', 'depth' => 1, 'childrens' => 
+                        [
+                            [
+                                'id' => '5', 'title' => 'Child 1.2.1', 'depth' => 2
+                            ],
+                            [
+                                'id' => '6', 'title' => 'Child 1.2.2', 'depth' => 2, 'childrens' => 
+                                [
+                                    [
+                                        'id' => '74', 'title' => '1.2.2.1', 'depth' => 3, 'childrens' => 
+                                        [
+                                            ['id' => '8', 'title' => 'Child 1.2.2.1.1', 'depth' => 4],
+                                            ['id' => '9', 'title' => 'Child 1.2.2.1.2', 'depth' => 4]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
             ['id' => '10', 'title' => 'Parent 3', 'depth' => 0]
         ];
 
